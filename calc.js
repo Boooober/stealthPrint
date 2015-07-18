8b612198fddcd42c2d84df710edc6220e45d3a7e
Nikita Slobodian <Boooober@ukr.net>
2015-07-18 13:04:33 +0300

    Formats input

--- calc.js
+++ calc.js
@@ -1,56 +1,40 @@
 window.addEventListener("load", function () {
 	
 	
+	
+	var calcForm = document.getElementById('sptCalc');
+	var selectFormat = document.getElementById('format');
+	
+	selectFormat.addEventListener('change', function(){
+		setFormat(selectFormat.options[selectFormat.selectedIndex].value);	
+	});
+	
+	function setFormat(format){
+		
+		var formats = {
+			A0:{"width":120,"height":84},
+			A1:{"width":84,"height":60},
+			A2:{"width":60,"height":42},
+			A3:{"width":42,"height":30},
+			A4:{"width":30,"height":21},
+			A5:{"width":21,"height":15}
+		};
+		
+		if (format == "custom") return;
+		
+		document.getElementById('calcFormWidth').value = formats[format]['width'];
+		document.getElementById('calcFormHeight').value = formats[format]['height'];
+	};
 
 	calcForm.addEventListener('submit', function(e){
 		e.preventDefault();
 		
-		getData();	
+		sendData();
+		
 	});
 	
 	function sendData(){
-		
-		var calcForm = document.getElementById('sptCalc');
-		var formatCustom = calcForm.querySelector('.formatCustom');
-		var formatA = calcForm.querySelector('.formatA');
-
+		var formData = new FormData(calcForm);
+		console.log(formData);
 	}
-	
-	
-	
 });
-/*
-window.addEventListener("load", function () {
-  function sendData() {
-    var XHR = new XMLHttpRequest();
-
-    // We bind the FormData object and the form element
-    var FD  = new FormData(form);
-
-    // We define what will happen if the data are successfully sent
-    XHR.addEventListener("load", function(event) {
-      alert(event.target.responseText);
-    });
-
-    // We define what will happen in case of error
-    XHR.addEventListener("error", function(event) {
-      alert('Oups! Something goes wrong.');
-    });
-
-    // We setup our request
-    XHR.open("POST", "http://ucommbieber.unl.edu/CORS/cors.php");
-
-    // The data sent are the one the user provide in the form
-    XHR.send(FD);
-  }
- 
-  // We need to access the form element
-  var form = document.getElementById("myForm");
-
-  // to takeover its submit event.
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    sendData();
-  });
-});*/
